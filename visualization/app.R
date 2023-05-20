518e679f756690efb2751168847ed5aa90de9dd1
damontano <127995333+damontano@users.noreply.github.com>
2023-05-20 14:09:37 -0500

    Update app.R

--- visualization/app.R
+++ visualization/app.R
@@ -15,42 +15,53 @@ ui <- dashboardPage(
   dashboardSidebar(
     fileInput("count_table", "Upload count table"),
     actionButton("normalization", "Normalize Data")
-  ),
+    ),
+  
   # Setting the layout for the plots that will get displayed
   dashboardBody(
-    fluidRow(
-      box(plotOutput("raw_plot", height = 400), width = 6),
-      box(plotOutput("normalized_plot", height = 400), width = 6)
-    ),
-    fluidRow(
-      box(plotlyOutput("pca_plot", height = 400), width = 6),
-      box(plotlyOutput("box_plot", height = 400), width = 6)
-    )  
+    tabsetPanel(
+      tabPanel("Counts Table", dataTableOutput("data_table")
+               ),
+      tabPanel("Plots",
+               fluidRow(
+                 box(plotOutput("raw_plot", height = 400), width = 6),
+                 box(plotOutput("normalized_plot", height = 400), width = 6)
+                 ),
+               fluidRow(
+                 box(plotlyOutput("pca_plot", height = 400), width = 6),
+                 box(plotlyOutput("box_plot", height = 400), width = 6)
+                 )
+               ),
+      )
+    )
   )
-)
 
 # Initializing server to create plots
 server <- function(input, output) {
-  
-  # Loading count table from user input file and processing it to use it 
+  # Loading count table from user input file and processing it to use it
   # in the next steps
   data <- reactive({
     req(input$count_table)
     inFile <- input$count_table
     data <- read.table(inFile$datapath, header = TRUE, row.names = 1)
-    
+
     # Log transforming the data to avoid the insertion of negative numbers
     data <- data + 1
     data <- log2(data)
     return(data)
   })
   
-  # Creating a density plot for raw count table 
+  output$data_table <- renderDataTable({
+    req(data())
+    data()
+    })
+
+  # Creating a density plot for raw count table
   output$raw_plot <- renderPlot({
     counts <- data()
     plot(density(colSums(counts)), main = "Raw Count Data", xlab = "Total Counts", ylab = "Density")
   })
-  
+
   # Normalizing count table and creating the plot
   normalized_count <- eventReactive(input$normalization, {
     counts <- data()
@@ -65,8 +76,8 @@ server <- function(input, output) {
     row.names(n) <- row.names(norm_counts)
     list(norm_counts = norm_counts, E = n)
   })
-  
-  # Creating a density plot for normalized count table 
+
+  # Creating a density plot for normalized count table
   output$normalized_plot <- renderPlot({
     norm_counts <- normalized_count()$norm_counts
     v <- voom(norm_counts, design = NULL, plot = FALSE)
@@ -75,7 +86,7 @@ server <- function(input, output) {
     norm_data
     plot(density(colSums(norm_data)), main = "Normalized Count Data", xlab = "Total Counts", ylab = "Density")
   })
-  
+
   # Creating PCA plot using plotly for an interactive view
   output$pca_plot <- renderPlotly({
     norms <- normalized_count()$n
@@ -89,12 +100,12 @@ server <- function(input, output) {
              yaxis = list(title = paste("PC2 (", summary(pca)$percentage[2, 1], "%)", sep="")))
     p
   })
- 
+
   # Creating boxplot using plotly for an interactive view
   output$box_plot <- renderPlotly({
     norm_counts <- normalized_count()$norm_counts
     boxplot_df <- data.frame(Sample = colnames(norm_counts), Counts = as.numeric(as.vector(t(norm_counts))))
-   b <- plot_ly(boxplot_df, x = ~Sample, y = ~log10(Counts), type = "box") %>%
+    b <- plot_ly(boxplot_df, x = ~Sample, y = ~log10(Counts), type = "box") %>%
       layout(title = "Box plot", xaxis = list(title = "Sample"), yaxis = list(title = "log10 Counts"))
     b
   })
