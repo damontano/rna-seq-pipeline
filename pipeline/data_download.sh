14d01b23c84442241f20991c393122900080fef7
damontano <127995333+damontano@users.noreply.github.com>
2023-05-21 15:12:51 -0500

    Update data_download.sh

--- pipeline/data_download.sh
+++ pipeline/data_download.sh
@@ -5,6 +5,7 @@ mkdir fastq_files
 cd fastq_files
 
 # Download short version of fastq files as specified in
+# https://training.galaxyproject.org/training-material/topics/transcriptomics/tutorials/rna-seq-reads-to-counts/tutorial.html#mapping
 for i in `seq 2444 2455`; do
     wget "https://zenodo.org/record/4249555/files/SRR155$i.fastq.gz"
 done
